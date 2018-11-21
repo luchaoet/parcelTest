680ded417cf536b0e741f9f0e0a28401462cf51b
luc <550502661@qq.com>
2018-11-21 23:04:48 +0800

    feat:parcel搭建React项目

--- parcel-react/src/App.js
+++ parcel-react/src/App.js
@@ -1,10 +1,19 @@
 import React, { Component } from 'react';
-import './App.css';
+import './App.scss';
+import { BrowserRouter as Router, Route,Redirect } from 'react-router-dom';
+import About from './pages/About/index'
+import Index from './pages/Index/index'
 
 export default class App extends Component {
   render() {
-    return <div>
-      <h1>Hello World 📦 🚀</h1>
-    </div>;
+    return (
+      <Router>
+          <div className="app_pages_wrap">
+            <Route exact path="/" render={ () => ( <Redirect to="/index" /> ) } />
+            <Route path="/index" component={Index}/>
+            <Route path="/about" component={About}/>
+          </div>
+      </Router>
+    )
   }
 }
